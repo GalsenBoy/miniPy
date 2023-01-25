6f53fbd54e6f1e2e35ad604dfa3d3f230369f982
bilaly <billycsk@gmail.com>
2023-01-25 11:12:58 +0100

    Gestion de produit avec une liste

--- src/list.py
+++ src/list.py
@@ -1,6 +1,37 @@
-course = [
-    ["Mangue", "Pomme", "Ananas", "Melon", "Pastèque"],
-    ["Viande", "Poisson", "Chou", "Carotte"]
-]
+basket = []
 
-print(course[0][1])
+print("1: Rajouter un produit dans votre panier" + "\n" +
+      "2: Supprimer un produit de votre liste" + "\n" +
+      "3: Voir la liste de votre panier" + "\n" +
+      "4: Vider votre panier" + "\n" +
+      "5: Sortir de l'application")
+
+choice = 0
+
+while choice > 1 or choice <= 5:
+    choice = int(input("Que voulez-vous faire" + "\n"))
+    if choice == 1:
+        add = input("Veuillez entrer le produit à ajouter")
+        basket.append(add)
+        print(f"Votre produit {add} a été ajouté avec succès dans votre panier")
+    elif choice == 2:
+        delete = input("Veuillez saisir le produit à supprimer")
+        if delete in basket:
+            basket.remove(delete)
+            print(f"Le produit {delete} a été supprimé avec succès dans votre panier")
+        else:
+            print("Le produit n'existe pas dans votre panier")
+
+    elif choice == 3:
+        for index, value in enumerate(basket, 1):
+            print(index, value)
+
+    elif choice == 4:
+        basket.clear()
+
+    else:
+        exit()
+
+
+
+
--- src/myster.py
+++ src/myster.py
@@ -1,12 +1,11 @@
 import random
 myster = random.randint(1, 100)
 
-saisi = int(input("Essayer de deviner le nombre" + "\n"))
-print(saisi)
+saisi = 0
 #print(f"le nombre à deviner est {myster}")
 i = 0
-while i < 2 and myster != saisi:
-    saisi = int(input("Réessayer de deviner le nombre" + "\n"))
+while i < 3 and myster != saisi:
+    saisi = int(input("Essayer de deviner le nombre" + "\n"))
     print(saisi)
     i += 1
     if myster > saisi:
@@ -18,7 +17,7 @@ while i < 2 and myster != saisi:
         print("Vous avez deviner le nombre")
         exit()
 
-    if i == 2:
+    if i == 3:
         print("Vous avez perdu")
         print(f"le nombre à deviner était {myster}")
 
