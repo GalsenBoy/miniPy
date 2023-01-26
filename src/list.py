d13d590429f166553354600a95016e3add04c19c
bilaly <billycsk@gmail.com>
2023-01-26 11:11:31 +0100

    Rajout de la fonctionnalité vider le panier plus optimisation du code

--- src/list.py
+++ src/list.py
@@ -2,33 +2,37 @@ import json
 
 basket = "C:/Users/Cissokho/Documents/data.json"
 
-print("1: Rajouter un produit dans votre panier" + "\n" +
-      "2: Supprimer un produit de votre liste" + "\n" +
-      "3: Voir la liste de votre panier" + "\n" +
-      "4: Vider votre panier" + "\n" +
-      "5: Sortir de l'application")
+
+def print_choice():
+    print("1: Rajouter un produit dans votre panier \n" +
+          "2: Supprimer un produit de votre liste \n" +
+          "3: Voir la liste de votre panier \n" +
+          "4: Vider votre panier \n" +
+          "5: Sortir de l'application")
+
+
+print_choice()
 
 choice = 0
-
 while choice > 1 or choice <= 5:
-    choice = int(input("Que voulez-vous faire" + "\n"))
+    choice = int(input("Que voulez-vous faire \n"))
     if choice == 1:
-        add = input("Veuillez entrer le produit à ajouter" + "\n")
+        add = input("Veuillez entrer le produit à ajouter \n")
         with open(basket, "r", encoding="utf-8") as file:
             data = json.load(file)
             data.append(add)
         with open(basket, "w") as file:
             json.dump(data, file, indent=4)
-        print(f"Votre produit {add} a été ajouté avec succès dans votre panier")
+        print(f"Votre produit {add} a été ajouté avec succès dans votre panier \n")
     elif choice == 2:
-        delete = input("Veuillez saisir le produit à supprimer" + "\n")
+        delete = input("Veuillez saisir le produit à supprimer \n")
         with open(basket, "r", encoding="utf-8") as file:
             data = json.load(file)
             if delete in data:
                 data.remove(delete)
-                print(f"Le produit {delete} a été supprimé avec succès dans votre panier")
+                print(f"Le produit {delete} a été supprimé avec succès dans votre panier \n")
             else:
-                print("Le produit n'existe pas dans votre panier")
+                print("Le produit n'existe pas dans votre panier \n")
 
         with open(basket, "w", encoding="utf-8") as file:
             json.dump(data, file)
@@ -36,11 +40,21 @@ while choice > 1 or choice <= 5:
     elif choice == 3:
         with open(basket, "r", encoding="utf-8") as file:
             data = json.load(file)
-            for index, value in enumerate(data, 1):
-                print(index, value)
+            if data:
+                for index, value in enumerate(data, 1):
+                    print(index, value)
+            else:
+                print("Votre liste est vide \n")
 
     elif choice == 4:
-        basket.clear()
+        with open(basket, "r", encoding="utf-8") as file:
+            data = json.load(file)
+            data.clear()
+        with open(basket, "w") as file:
+            json.dump(data, file)
 
+        print("Votre liste a été vidé avec succès \n")
     else:
+        print("A bientôt")
         exit()
+    print_choice()
